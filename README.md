b102a14a23f5dcfacde7a3738b6d6658c5082f4c
anantkolvankar <ani.kolvankar@gmail.com>
2015-10-02 14:30:05 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Grails Doc Search
+
+
+This extension for Google Chrome provides a way to go to a particular
+[Grails Doc](https://grails.org/) directly using grails version from the omnibar.
+
+## Usage
+
+ 1. Type `grailsdoc` into the omnibar then hit `tab` or `space` to
+    initialize the extension.
+ 2. Type complete grails version eg `2.0.0` and press `Enter`/`Return` this will take you to documentation page of that particular grails version.
---
+++ background.js
@@ -0,0 +1,26 @@
+function resetDefaultSuggestion() {
+  chrome.omnibox.setDefaultSuggestion({
+    description: 'Go to grails doc version: %s'
+  });
+}
+
+resetDefaultSuggestion();
+
+chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
+  // Suggestion code will end up here.
+});
+
+chrome.omnibox.onInputCancelled.addListener(function() {
+  resetDefaultSuggestion();
+});
+
+function navigate(url) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.update(tabs[0].id, {url: url});
+  });
+}
+
+chrome.omnibox.onInputEntered.addListener(function(text) {
+  navigate("http://grails.github.io/grails-doc/" + text);
+});
+
